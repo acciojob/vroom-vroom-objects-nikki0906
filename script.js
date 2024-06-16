087f504ce1c594a66f94fe595bc083f9aeb460e7
nikki0906 <160705654+nikki0906@users.noreply.github.com>
2024-06-16 11:51:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,31 @@
 // Complete the js code
-function Car(make, model) {}
+function Car(make, model) {
+  this.make = make;
+  this.model = model;
+}
 
-function SportsCar(make, model, topSpeed) {}
+Car.prototype.getMakeModel = function() {
+  return `${this.make} ${this.model}`;
+};
 
-// Do not change the code below
+function SportsCar(make, model, topSpeed) {
+  Car.call(this, make, model); // Call the Car constructor
+  this.topSpeed = topSpeed;
+}
+
+// Inherit the Car prototype
+SportsCar.prototype = Object.create(Car.prototype);
+SportsCar.prototype.constructor = SportsCar;
+
+SportsCar.prototype.getTopSpeed = function() {
+  return this.topSpeed;
+};
+
+// Example test case
+const car = new SportsCar("Ferrari", "Testarossa", 200);
+console.log(car.getMakeModel()); // Output: Ferrari Testarossa
+console.log(car.getTopSpeed()); // Output: 200
+
+// Do not change code below this line
 window.Car = Car;
 window.SportsCar = SportsCar;
-
