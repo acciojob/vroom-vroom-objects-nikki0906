8ac9b091e3a0b5806f475922adc305923e6a3381
nikki0906 <160705654+nikki0906@users.noreply.github.com>
2024-06-16 11:48:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,9 @@
 // Complete the js code
-describe('Car and SportsCar Classes', () => {
-  it('should create a Car and call getMakeModel method', () => {
-    const car = new window.Car('Toyota', 'Corolla');
-    expect(car.getMakeModel()).to.equal('Toyota Corolla');
-  });
+function Car(make, model) {}
 
-  it('should create a SportsCar and call getMakeModel and getTopSpeed methods', () => {
-    const sportsCar = new window.SportsCar('Ferrari', 'Testarossa', 200);
-    expect(sportsCar.getMakeModel()).to.equal('Ferrari Testarossa');
-    expect(sportsCar.getTopSpeed()).to.equal(200);
-  });
-});
+function SportsCar(make, model, topSpeed) {}
+
+// Do not change the code below
+window.Car = Car;
+window.SportsCar = SportsCar;
+
